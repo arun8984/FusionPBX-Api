a62bd940c37e758e172cda4c82959b8bd6490574
Arunprakash N <nap8984@gmail.com>
2020-07-09 22:36:38 +0530

    auth added

--- restapi/extensions/encrypt_value.php
+++ restapi/extensions/encrypt_value.php
@@ -1,4 +1,9 @@
 <?php
+header('Content-Type: application/json');
+
+include '/var/www/fusionpbx/root.php';
+require_once '/var/www/fusionpbx/resources/check_auth.php';
+
 include 'security.php';
 define('KEY_SECURE','h5r@mg7$#ueqdstj');
 $source = Security::encrypt($_GET['source']), KEY_SECURE);
