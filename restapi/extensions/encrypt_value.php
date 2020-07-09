049024dfaac9d4ced6c9eac8946b678b7234d05c
Arunprakash N <nap8984@gmail.com>
2020-07-09 22:39:58 +0530

    syntax error

--- restapi/extensions/encrypt_value.php
+++ restapi/extensions/encrypt_value.php
@@ -6,7 +6,7 @@ require_once '/var/www/fusionpbx/resources/check_auth.php';
 
 include 'security.php';
 define('KEY_SECURE','h5r@mg7$#ueqdstj');
-$source = Security::encrypt($_GET['source']), KEY_SECURE);
+$source = Security::encrypt($_GET['source'], KEY_SECURE);
 echo json_encode(array("result"=>"success" , 'value'=>$source ));
 die;
 
