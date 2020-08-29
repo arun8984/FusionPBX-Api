997d813f0880d3b2fe06bf14cc697d9b0f1e42e3
root <root@208.70.254.8>
2020-08-29 06:53:03 -0400

    Push Token Update

--- restapi/extensions/updatetoken.php
+++ restapi/extensions/updatetoken.php
@@ -6,16 +6,23 @@ set_time_limit(3600);
 $domain_uuid = (isset($_GET['domain_uuid']) ? $_GET['domain_uuid'] : null);
 $extension = (isset($_GET['extension']) ? $_GET['extension'] : null);
 $token = (isset($_GET['token']) ? $_GET['token'] : null);
-
-if ($domain_uuid !== null and $extension !== null and $token !== null) {
+$os = (isset($_GET['os']) ? $_GET['os'] : null);
+if ($domain_uuid !== null and $extension !== null and $token !== null and $os !== null) {
 	
-    $sql = "select count(*) as ExtCount from devices where extension = '$extension' and extendomain = '$domain_uuid' ";
+    $sql = "select * from devices where extension = '$extension' and domain_uuid = '$domain_uuid' ";
     $prep_statement = $db->prepare(check_sql($sql));
     $prep_statement->execute();
     $result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
     unset($sql);
-    print_r($result)
+    if (sizeof($result) == 0){
+	$sql = "INSERT INTO devices (extension, domain_uuid, os, pushtoken)VALUES(:extension, :domain_uuid, :os, :token)";
+    }else{
+	$sql="UPDATE devices set pushtoken = :token, os = :os where extension = :extension and domain_uuid = :domain_uuid";
     }
+    $prep_statement = $db->prepare($sql);
+    $prep_statement->execute(['extension'=>$extension,'domain_uuid'=>$domain_uuid,'os'=>$os,'token'=>$token]);
+    
+    
 }
 
 ?>
